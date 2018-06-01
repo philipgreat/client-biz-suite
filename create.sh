cec31ad1495d4bb8f0dc252f0040223e900a33c7
Philip Z <philip_chang@163.com>
2018-06-01 15:12:10 +0800

    Update create.sh

--- create.sh
+++ create.sh
@@ -4,9 +4,9 @@ projectname="$1-biz-suite"
 git clone https://github.com/philipgreat/client-biz-suite.git  $projectname 
 rm -rf $projectname/.git
 echo "$projectname" >> $projectname/projectname
-echo "copying bizcore.........."
+echo "copying bizcore..........$projectname/bizcore/"
 cp -Ra skynet-common-app-framework/*  "$projectname/bizcore/"
-echo "copying bizui.........."
+echo "copying bizui..........to $projectname/bizui/"
 cp -Ra skynet-react-framework/* "$projectname/bizui/"
 echo "done"
 sed -i -e  "s/__project_name__/$projectname/g" $projectname/gitinit.sh 
