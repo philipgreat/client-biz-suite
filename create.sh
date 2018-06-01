3553c058b4bf3a41f97e2e9ad17b5be3431fafad
Philip Z <philip_chang@163.com>
2018-06-01 14:25:28 +0800

    Update create.sh

--- create.sh
+++ create.sh
@@ -2,5 +2,6 @@
 
 projectname="$1-biz-suite "
 git clone https://github.com/philipgreat/client-biz-suite.git  $projectname 
-rm -rf kaisi-biz-suite/.git && cp -Ra skynet-common-app-framework/*  $projectname/bizcore/ 
+rm -rf $projectname/.git
+cp -Ra skynet-common-app-framework/*  $projectname/bizcore/ 
 cp -Ra skynet-react-framework/* $projectname/bizui/
