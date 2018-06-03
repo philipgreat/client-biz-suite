8f2bd077c9b2374c97569e0d2d6643d64d063072
Philip Z <philip_chang@163.com>
2018-06-04 01:12:00 +0800

    Update gitinit.sh

--- gitinit.sh
+++ gitinit.sh
@@ -2,7 +2,7 @@
 projectname="__project_name__"
 echo "# $projectname " >> README.md
 git init
-git add README.md
+git add -A .
 git commit -m "first commit"
 git remote add origin https://github.com/philipgreat/$projectname.git
 git push -u origin master
