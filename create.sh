455d609f71485e7cbcbf248c2e90a0d7dd3a53e2
philipgreat <philip_chang@163.com>
2018-06-07 17:37:09 +0800

    move folders

--- create.sh
+++ create.sh
@@ -6,8 +6,10 @@ rm -rf $projectname/.git
 echo "$projectname" >> $projectname/projectname
 echo "copying bizcore..........$projectname/bizcore/"
 cp -Ra skynet-common-app-framework/*  "$projectname/bizcore/"
+mv "$projectname/bizcore/viewlayers/*" "$projectname/bizcore/"
+mv "$projectname/bizcore/static/*" "$projectname/bizcore/"
 echo "copying bizui..........to $projectname/bizui/"
-cp -Ra skynet-react-framework/* "$projectname/bizui/"
+cp -Ra skynet-react-framework/. "$projectname/bizui/"
 echo "repalcing __project_name__ to $projectname in gitinit.sh"
 sed -i -e  "s/__project_name__/$projectname/g" $projectname/gitinit.sh 
 echo "done"
