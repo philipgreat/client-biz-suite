757decfb6550c97173f0f92abbfc558f24553a68
Philip Z <philip_chang@163.com>
2018-07-21 01:19:59 +0800

    Update create.sh

--- create.sh
+++ create.sh
@@ -12,6 +12,8 @@ mv $projectname/bizcore/statics/* $projectname/bizcore
 echo "copying bizui..........to $projectname/bizui/"
 cp -Ra skynet-react-framework/. "$projectname/bizui/"
 rm -rf "$projectname/bizui/.git"
+rm -rf "$projectname/bizcore/.classpath"
+rm -rf "$projectname/bizcore/.project"
 echo "repalcing __project_name__ to $projectname in gitinit.sh"
 sed -i -e  "s/__project_name__/$projectname/g" $projectname/gitinit.sh 
 echo "repalcing __project_name__ to $projectname in deploy.sh"
