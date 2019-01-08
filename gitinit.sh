3a24f7940d5414469fd18a01a8ac19585ef9b06a
Philip Z <philip_chang@163.com>
2019-01-08 18:22:30 +0800

    Update gitinit.sh

--- gitinit.sh
+++ gitinit.sh
@@ -4,5 +4,5 @@ echo "# $projectname " >> README.md
 git init
 git add -A .
 git commit -m "first commit"
-git remote add origin https://github.com/doublechain/$projectname.git
+git remote add origin https://github.com/doublechaintech/$projectname.git
 git push -u origin master
